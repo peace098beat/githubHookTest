06d3c36ecc8c47db8c43c5c5a85fea15838aa07b
野原 友幸 <peace098beat.t@gmail.com>
2016-02-01 22:43:32 +0900

    add auto-dep..php

--- test/git-Auto-Deploy.php
+++ test/git-Auto-Deploy.php
@@ -1,6 +1,6 @@
 <?php
 
-exec('cd /home/fififactory/githubHookTest/');
+exec('cd /home/fififactory/www/githubHookTest/');
 exec('git --git-dir=.git pull');
 exec('echo "SUSSSS" > fifi.log');
 echo 'fifi';
